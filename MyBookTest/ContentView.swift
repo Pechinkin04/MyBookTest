a0290d7e274dcef8376c2b5d9f152f4911b6e9e4
Alexander Pechinkin <pechinkin04@yandex.ru>
2024-08-14 15:28:49 +0400

    Initial Commit

---
+++ MyBookTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyBookTest
+//
+//  Created by Александр Печинкин on 14.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyBookTest/MyBookTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBookTestApp.swift
+//  MyBookTest
+//
+//  Created by Александр Печинкин on 14.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyBookTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
